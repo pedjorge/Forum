2740a701efb640a8718c2b8812da033f1192333d
Pedro Jorge <pedrojorge2@hotmail.com>
2014-09-29 14:40:44 +0100

    fix nativation

--- topic.php
+++ topic.php
@@ -2,26 +2,47 @@
 <html>
     <head>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/global.css">
         <link rel="stylesheet" href="css/topic.css">
-        
         <script src="js/jquery_popup.js"></script>
     </head>
     <body>
-
-        <div id="header_container">
+        <div id="menu">
             <div id="header">
                 <div class="logo_title">
-                    <h1><em><b>Forum</b></em></h1>
+                    <div class="logo">
+                        <a href="#"><img class="logo" src="img/logo.png"></a>
+                    </div>
+                    <div class="title">
+                        <a href="home.php"><h1><em><b>Forum</b></em></h1></a>
+                    </div>
                 </div>
-
                 <div class="search_container">
                     <input type="search" id="mySearch" placeholder="Search for something..">
                     <button onclick="myFunction()">Search</button>
                 </div>
             </div>
+            <div id="navigation">
+                <ul id="navigation_items">
+                    <li class="Starred_topics menu_item">
+                        <a href="#" class="menu_item_link"><p>Starred topics</p></a>
+                    </li>
+                    <li class="Forums menu_item">
+                        <a href="#" class="menu_item_link"><p>Forums</p></a>
+                    </li>
+                    <li class="Topics menu_item">
+                        <a href="#" class="menu_item_link"><p>Topics</p></a>
+                    </li>
+                    <li class="Settings menu_item">
+                        <a href="#" class="menu_item_link"><p>Settings</p></a>
+                    </li>
+                </ul>
+            </div>
+            <div id="footer">
+                <p>&#169; 2014 First Last</p>
+            </div>
         </div>
-
         <div id="container">
             <div id="content">
             <?php include 'php_support/show_posts.php'; ?>
@@ -38,12 +59,5 @@
             </div>
             <div class="push"></div>
         </div>
-        <footer>
-            <ul>
-                <li class="first">
-
-                </li>
-            </ul>
-        </footer>
     </body>
 </html>
