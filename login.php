634c7ed7121e2c877c72ac7e636364c31125c564
Pedro Jorge <pedrojorge2@hotmail.com>
2014-10-01 12:13:59 +0100

    login ajax

--- login.php
+++ login.php
@@ -1,8 +1,78 @@
+<!DOCTYPE html>
+<?php
+    // continue the session
+    session_start();
+?>
 <html>
     <head>
         <title>User Login Form</title>
         <link rel="stylesheet" href="css/login-register.css">
         <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script type="text/javascript">
+            // create the XMLHttpRequest object, according browser
+            function get_XmlHttp() {
+              // create the variable that will contain the instance of the XMLHttpRequest object (initially with null value)
+              var xmlHttp = null;
+
+              if(window.XMLHttpRequest) {   // for Forefox, IE7+, Opera, Safari, ...
+                xmlHttp = new XMLHttpRequest();
+              }
+              else if(window.ActiveXObject) { // for Internet Explorer 5 or 6
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+              }
+
+              return xmlHttp;
+            }
+
+        $(document).ready(function(){
+            $('#username').focus(); // Focus to the username field on body loads
+            $('#login').click(function(){ // Create `click` event function for login
+                var username = $('#username'); // Get the username field
+                var password = $('#password'); // Get the password field
+                var login_result = $('#login_result'); // Get the login result div
+                login_result.html('loading..'); // Set the pre-loader can be an animation
+                if(username.val() == ''){ // Check the username values is empty or not
+                    username.focus(); // focus to the filed
+                    login_result.html('<span class="error">Enter the username</span>');
+                    return false;
+                }
+                if(password.val() == ''){ // Check the password values is empty or not
+                    password.focus();
+                    login_result.html('<span class="error">Enter the password</span>');
+                    return false;
+                }
+                if(username.val() != '' && password.val() != ''){ // Check the username and password values is not empty and make the ajax request
+
+                      var request =  get_XmlHttp();   // call the function for the XMLHttpRequest instance
+
+                      // create pairs index=value with data that must be sent to server
+                      var  the_data = 'username='+username.val()+'&password='+password.val();
+
+                      request.open("POST", 'php_support/verify.php', true);     // set the request
+
+                      // adds  a header to tell the PHP script to recognize the data as is sent via POST
+                      request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                      request.send(the_data);   // calls the send() method with datas as parameter
+
+                      // Check request status
+                      // If the response is received completely, will be transferred to the HTML tag with tagID
+                      request.onreadystatechange = function() {
+                        if (request.responseText == "incorrect") {
+                            login_result.html('<span class="error">Username or Password Incorrect!</span>');
+                        }
+                        else if (request.responseText == "login") {
+                            window.location = 'home.php';
+                        }
+                        else if (request.responseText == "db") {
+                            alert('Problem with sql query' + request.responseText);
+                        }
+                      }
+                }
+                return false;
+            });
+        });
+        </script>
     </head>
     <body>
         <div class="logo_title">
@@ -14,47 +84,42 @@
             </div>
         </div> 
         <div id="container">
-            <h1>Login</h1>
-            <?php
-                if (!isset($_POST['submit'])){
-            ?>
-
-            <!-- The HTML login form -->
-            <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-                <p>Username: <input type="text" name="username" /></p>
-                <p>Password: <input type="password" name="password" /></p>
-             
-                <input type="submit" name="submit" value="Login" />
-                <a href="register.php">REGISTER</a>
-            </form>
-            
-            <?php
-                } else {
-                    require_once("php_support/db_const.php");
-                    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-                    # check connection
-                    if ($mysqli->connect_errno) {
-                        echo "<p>MySQL error no {$mysqli->connect_errno} : {$mysqli->connect_error}</p>";
-                        exit();
-                    }
-                 
-                    $username = $_POST['username'];
-                    $password = $_POST['password'];
-                 
-                    $sql = "SELECT * from users WHERE username LIKE '".$username."' AND password LIKE '".$password."' LIMIT 1";
-                    $result = $mysqli->query($sql);
-                    if (!$result->num_rows == 1) {
-                        echo "<p>Invalid username/password combination</p>";
-                    } else {
-                        echo "<p>Logged in successfully</p>";
-                        session_start();
-                        $_SESSION["username"] = $username;
-                        sleep(5);
-                        header('Location: home.php');
-                        // do stuffs
-                    }
-                }
-            ?>  
+            <form>
+                <table>
+                    <tr>
+                        <td colspan="2">
+                            <h1>Login</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div id="login_result"></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Username</td>
+                        <td>
+                            <input type="text" name="username" id="username" class="credentials" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>
+                            <input type="password" name="password" id="password" class="credentials" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name="login" id="login" class="as_button" value="Login &raquo;" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <a id="register" href="register.php">not registered yet?</a>
+                        </td>
+                    </tr>
+                </table>
+            </form>         
         </div>      
     </body>
 </html>
---
+++ php_support/verify.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+	require_once("db_const.php");
+	$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	if (!$con) {
+      die('Could not connect: ' . mysqli_error($con));
+    }
+    mysqli_select_db($con,"forum_db");
+
+    if(isset($_POST['username']) && $_POST['password']){ // Check the action `login`
+    	$username 		= $_POST['username']; // Get the username 
+    	$password 		= $_POST['password']; // Get the password and decrypt it
+    	$sql			= "SELECT * FROM users 
+                           WHERE username LIKE '".$username."' 
+                           AND password LIKE '".$password."' LIMIT 1"; // Check the table with posted credentials
+    	$result         = mysqli_query($con,$sql); // Get the number of rows
+        $row            = mysqli_fetch_array($result);
+
+    	if($row <= 0){ // If no users exist with posted credentials print 0 like below.
+    		echo "incorrect";
+    	} else {
+    		$_SESSION['userid'] 	= $row['user_ID'];
+    		$_SESSION['username'] 	= $row['username'];
+    		echo "login";
+    	}
+    }
+?>
