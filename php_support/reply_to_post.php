d285ed9b8ebf2a8b999ab802060b2b8f1575d5ff
Pedro Jorge <pedrojorge2@hotmail.com>
2014-10-17 16:04:03 +0100

    clean up

--- php_support/reply_to_post.php
+++ php_support/reply_to_post.php
@@ -9,8 +9,19 @@
 	$date = $_GET['date'];
 	$post_ID = $_GET['post_ID'];
 
-	$sql_query_1="INSERT INTO replies (post_ID, user_ID, reply, date_created) 
-		  VALUES ('".$post_ID."', '".$_SESSION['user_ID']."','".$message."', '".date('Y-m-d H:i:s', strtotime($date))."')";
-	mysqli_query($con,$sql_query_1);
+	$sql = "SELECT
+		    	topic_ID
+		   	FROM 
+		   		posts 
+		   	WHERE 
+		   		post_ID = '".$post_ID."'";
+
+    $result = mysqli_query($con,$sql);
+    $row = mysqli_fetch_array($result);
+    $topic_ID = $row['topic_ID']; 
+
+	$sql_query_2="INSERT INTO replies (post_ID, user_ID, topic_ID, reply, date_created) 
+		  VALUES ('".$post_ID."', '".$_SESSION['user_ID']."', '".$topic_ID."', '".$message."', '".date('Y-m-d H:i:s', strtotime($date))."')";
+	mysqli_query($con,$sql_query_2);
 	mysqli_close($con);
 ?>
